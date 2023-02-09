a9fb637017b41d28d849a4aada69ad06e6262303
Ecoder-JianZhou <ecoder-zj@outlook.com>
2023-02-09 16:55:26 -0500

    1

--- 2_fortran/2_1_write_time_into_nc.f90
+++ 2_fortran/2_1_write_time_into_nc.f90
@@ -62,3 +62,65 @@ program write_time
    end if
 
 end program write_time
+
+program write_netcdf
+    use netcdf
+    implicit none
+    
+    integer :: ncid, time_dimid, time_varid, data_varid
+    integer :: start(1), count(1)
+    integer :: i, j, status
+    real, dimension(24) :: data
+    real :: time(24)
+ 
+    call check( nf90_create('test.nc', NF90_CLOBBER, ncid) )
+ 
+    ! Define the time dimension
+    call check( nf90_def_dim(ncid, 'time', 24, time_dimid) )
+ 
+    ! Define the time variable
+    call check( nf90_def_var(ncid, 'time', NF90_DOUBLE, 1, &
+                             time_dimid, time_varid) )
+    call check( nf90_put_att(ncid, time_varid, 'units', &
+                             'hours since 2000-01-01 00:00:00') )
+    call check( nf90_enddef(ncid) )
+ 
+    ! Write the time data
+    do i = 1, 24
+       time(i) = (i-1)/24.0
+    end do
+ 
+    start = 1
+    count = 24
+    call check( nf90_put_var(ncid, time_varid, time, start, count) )
+ 
+    ! Define the data variable
+    call check( nf90_redef(ncid) )
+    call check( nf90_def_var(ncid, 'data', NF90_FLOAT, 1, &
+                             time_dimid, data_varid) )
+    call check( nf90_put_att(ncid, data_varid, 'units', &
+                             'units of data') )
+    call check( nf90_enddef(ncid) )
+ 
+    ! Write the data
+    do i = 1, 24
+       data(i) = i*i
+    end do
+ 
+    start = 1
+    count = 24
+    call check( nf90_put_var(ncid, data_varid, data, start, count) )
+ 
+    call check( nf90_close(ncid) )
+ 
+    contain
+ 
+    subroutine check(status)
+       if (status /= NF90_NOERR) then
+          print *, nf90_strerror(status)
+          stop
+       end if
+    end subroutine check
+ 
+ end program write_netcdf
+ 
